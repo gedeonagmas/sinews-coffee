466ca0b269e49ea43ae91f825bc6595660a4a6d5
gedeonagmas <gedeonagmas2580@gmail.com>
2024-12-05 20:42:45 +0300

    updated

--- pages/contact.js
+++ pages/contact.js
@@ -169,9 +169,7 @@ const Contact = () => {
                   </div>
                   <div className="cda-content-inner">
                     <h4>Telephone Number One</h4>
-                    <p>
-                      {contacts?.phone1}
-                    </p>
+                    <p>{contacts?.phone1}</p>
                   </div>
                 </div>
                 <div className="cda-single-content hr d-flex">
@@ -180,9 +178,7 @@ const Contact = () => {
                   </div>
                   <div className="cda-content-inner">
                     <h4>Telephone Number Two</h4>
-                    <p>
-                      {contacts?.phone2}
-                    </p>
+                    <p>{contacts?.phone2}</p>
                   </div>
                 </div>
                 <div className="cda-single-content hr d-flex">
@@ -197,7 +193,6 @@ const Contact = () => {
                   </div>
                 </div>
 
-                
                 <div className="cda-single-content hr d-flex"></div>
               </div>
             </div>
@@ -212,14 +207,14 @@ const Contact = () => {
           <div className="row">
             <div className="col-lg-12 p-0">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48384.367867189205!2d-74.01058227968896!3d40.71751035716885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1609671967457!5m2!1sen!2sbd"
-                width={1920}
-                height={350}
-                style={{ border: 0 }}
-                allowFullScreen
-                aria-hidden="false"
-                tabIndex={0}
-              />
+                src={contacts?.gmapUrl}
+                width="1920"
+                height="350"
+                style={{ border: "0" }}
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>{" "}
             </div>
           </div>
         </div>
